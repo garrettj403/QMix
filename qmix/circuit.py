59d079a9d891735b71dab1cecbab8e450be72bb6
John Garrett <john.garrett@astro.ox.ac.uk>
2018-11-12 16:13:01 +0000

    Update docstrings to include all arguments

--- qmix/circuit.py
+++ qmix/circuit.py
@@ -1,4 +1,4 @@
-""" Embedding circuit / Thevenin equivalent circuit
+""" Thevenin equivalent circuit to represent the embedding circuit
 
 This module contains a class to contain all of the data about the embedding
 circuit (i.e., voltages/impedances/frequencies).
@@ -8,15 +8,14 @@ Note:
    - After creating an instance of the ``EmbeddingCircuit`` class, 3 class
      variables must be set:
 
-      - The **photon voltage (vph)** of each tone
+      - The **photon equivalent voltage (vph)** of each tone
       - The **Thevenin voltage (vt)** of each tone/harmonic
       - The **Thevenin impedance (zt)** of each tone/harmonic
 
-   - Each of these class variables must be set manually. They are all
-     one-based indexed:
+   - Each of these class variables must be set manually.
 
-      - e.g., to set the Thevenin voltage of the 2nd tone's 3rd harmonic to
-        0.3, you'd use:
+      - E.g., to set the Thevenin voltage of the 3rd harmonic of the 2nd tone 
+        to 0.3, you'd use:
 
          ``cct.vt[2,3] = 0.3``
 
@@ -30,16 +29,17 @@ import numpy as np
 
 from qmix.misc.terminal import cprint
 
+
 # EMBEDDING CIRCUIT CLASS ----------------------------------------------------
 
 
 class EmbeddingCircuit(object):
-    """Class to contain the embedding circuit. All voltages, impedances and
-    frequencies.
+    """Class to contain the embedding circuit. This includes all voltages, 
+    impedances and frequencies.
 
     Creating an instance of this class will set the sizes and data types
     of all of the variables, but the actual values will need to be set
-    manually. In this way, this is more of a struct than a class. The
+    manually. In this way, this class is sort of like a struct. The
     variables that must be set are:
 
        - ``cct.vph``:  photon voltage (freq / gap freq)
@@ -54,26 +54,27 @@ class EmbeddingCircuit(object):
         vb_min (float): Minimum bias voltage
         vb_max (float): Maximum bias voltage
         vb_npts (int): Number of bias voltage points
+        fgap (float): Gap frequency, in units [Hz]
+        vgap (float): Gap voltage, in units [V]
+        rn (float): Normal-state resistance, in units [ohms]
+        name (str): Name of this instance
 
     """
 
-    def __init__(self, num_f=1, num_p=1, vb_min=0, vb_max=2, vb_npts=201, fgap=None, vgap=None, rn=None, name=None):
+    def __init__(self, num_f=1, num_p=1, vb_min=0, vb_max=2, vb_npts=201, fgap=None, vgap=None, rn=None, name=''):
 
-        self.name = name  # used to identify the circuit
+        self.name = name  # used to identify the circuit (optional)
 
         # Check input
-        assert num_f in [1, 2, 3, 4], "Number of tones must be equal to 1, 2, 3, or 4."
-        assert num_p >= 1 and isinstance(num_p, int), "Number of harmonics must be an integer equal or greater than 1."
-        assert isinstance(vb_npts, int) and vb_npts >= 1, "vb_npts must be an integer greater or equal to 1."
-        assert vb_max >= vb_min, "vb_max must be larger than vb_min."
+        assert num_f in [1, 2, 3, 4], "Number of tones (num_f) must be equal to 1, 2, 3, or 4."
+        assert num_p >= 1 and isinstance(num_p, int), "Number of harmonics (num_p) must be an integer equal or greater than 1."
+        assert isinstance(vb_npts, int) and vb_npts >= 1, "Number of bias points (vb_npts) must be an integer greater or equal to 1."
+        assert vb_max >= vb_min, "Maximum voltage (vb_max) must be larger than the minimum voltage (vb_min)."
 
         # Number of tones/harmonics
         self.num_f = int(num_f)
         self.num_p = int(num_p)
         self.num_n = self.num_f * self.num_p
-        self._num_f = self.num_f
-        self._num_p = self.num_p
-        self._num_n = self.num_n
 
         # Junction information (optional)
         self.fgap = None
@@ -105,15 +106,13 @@ class EmbeddingCircuit(object):
 
     def __str__(self):
 
-        if self.name is not None:
-            return "Embedding circuit (NF:{}, NP:{}): {}".format(self.num_f, self.num_p, self.name)
-        else:
-            return "Embedding circuit (NF:{}, NP:{})".format(self.num_f, self.num_p)
+        return "Embedding circuit (NF:{}, NP:{}): {}".format(self.num_f, self.num_p, self.name)
 
     def __repr__(self):
 
         return self.__str__()
 
+    # @property
     def initialize_vj(self):
         """ Initialize junction voltage array
 
@@ -124,8 +123,8 @@ class EmbeddingCircuit(object):
         it is okay for this class to initialize ``vj`` since it has all the
         data about matrix sizes.
 
-        This function is useful when the embedding impedances are all 0, and
-        you want to set the voltage across the junction manually.
+        This function is useful when the embedding impedance aren't included, 
+        and you want to set the voltage across the junction manually.
 
         Returns:
             ndarray: Empty matrix to initializing vj
@@ -134,7 +133,7 @@ class EmbeddingCircuit(object):
 
         return np.zeros((self.num_f + 1, self.num_p + 1, self.vb_npts), dtype=complex)
 
-    def available_power(self, f, p=1):
+    def available_power(self, f=1, p=1, units='W'):
         """ Return available power of tone f and harmonic p.
 
         Note: Gap voltage and normal resistance must be set prior!
@@ -142,9 +141,10 @@ class EmbeddingCircuit(object):
         Args:
             f (int): tone
             p (int): harmonic
+            units (str): units for power, either 'W' or 'dBm'
 
         Returns:
-            float: available power (in W)
+            float: available power in specified units
 
         """
 
@@ -154,11 +154,16 @@ class EmbeddingCircuit(object):
         v_v = self.vt[f, p] * self.vgap
         r_ohms = self.zt[f, p].real * self.rn
 
-        power = np.abs(v_v)**2 / r_ohms / 8
+        power = np.abs(v_v) ** 2 / r_ohms / 8.
 
-        return power
+        if units.lower() == 'w':
+            return power
+        elif units.lower() == 'dbm':
+            return 10 * np.log10(power * 1e3)
+        else:
+            raise ValueError('Not a recognized unit for power.')
 
-    def set_available_power(self, power, f, p=1):
+    def set_available_power(self, power, f=1, p=1, units='W'):
         """ Set available power of tone f and harmonic p.
 
         Note: Gap voltage, normal resistance and embedding impedance must be
@@ -168,37 +173,47 @@ class EmbeddingCircuit(object):
             power (float): power in W
             f (int): tone
             p (int): harmonic
+            units (str): units for power, either 'W' or 'dBm'
 
         """
 
         assert self.vgap is not None, 'Gap voltage not set'
         assert self.rn is not None, 'Normal resistance not set'
-        assert self.zt[f, p] != 0, 'Impedance not set'
+        assert self.zt[f, p] != 0, 'Embedding impedance not set'
+
+        if units.lower() == 'w':
+            pass
+        elif units.lower() == 'dbm':
+            power = 10 ** (power / 10) * 1e-3
+        else:
+            raise ValueError('Not a recognized unit for power.')
 
         r_ohms = self.zt[f, p].real * self.rn
         volt_v = np.sqrt(8 * power * r_ohms)
 
         self.vt[f, p] = volt_v / self.vgap
 
-    def set_alpha(self, alpha, f, p, zj=2. / 3):
+    def set_alpha(self, alpha, f=1, p=1, zj=2. / 3):
         """ Set drive level of tone f and harmonic p.
 
         Note: Gap voltage and normal resistance must be set prior!
 
-        This method guesses what vt should be in order to get the desired
-        drive level.
+        This method guesses what source voltage should be in order to get the 
+        desired drive level.
 
         Args:
-            alpha (float): alpha (= voltage / vph)
+            alpha (float): drive level, alpha = voltage / vph
             f (int): tone
             p (int): harmonic
             zj (float): estimated junction impedance
 
         """
 
+        assert self.zt[f, p] != 0, 'Embedding impedance not set'
+
         self.vt[f, p] = alpha * self.vph[f] * (self.zt[f, p] + zj) / zj
 
-    def set_vph(self, freq, f):
+    def set_vph(self, freq, f=1):
         """ Set normalized photon voltage of tone f.
 
         Args:
@@ -207,9 +222,11 @@ class EmbeddingCircuit(object):
 
         """
 
+        assert self.fgap is not None, 'Gap frequency not set'
+
         self.vph[f] = freq / self.fgap
 
-    def set_name(self, name, f, p):
+    def set_name(self, name, f=1, p=1):
         """ Set name of tone/harmonic.
 
         This has no effect on the actual sim. Just for keeping track
@@ -217,6 +234,8 @@ class EmbeddingCircuit(object):
 
         Args:
             name (str): name of tone/harmonic
+            f (int): frequency number to set
+            p (int): harmonic number to set
 
         """
 
@@ -263,50 +282,31 @@ class EmbeddingCircuit(object):
                     print(str8.format(power_dbm))
         print("")
 
-    def save_info(self, filename):
-        """Save information about the embedding circuit.
+    def save_info(self, filename='embedding-circuit.txt'):
+        """Save information about the embedding circuit to a text file.
 
         Args:
-            filename (string): File to save information to
+            filename (string): Filename
 
         """
 
         str1 = "   f={0}, p={1}\t\t\tvph = {2:.4f} x {1}\n"
-        str2 = "   f={0}, p={1}\t\t\t{2:.1f} GHz x {1}\n"
-        str3 = "\tThev. voltage:\t\t{:.4f} V / V_gap\n"
-        str6 = "\t   or alpha:    \t{:.4f}\n"
-        str4 = "\tThev. impedance:\t{:.2f} ohms / R_N\n"
-        str5 = "\tAvail. power:   \t{:7.1e} nW\n"
-        str7 = "\t                \t{:.3f} dBm\n"
+        str2 = "\tThev. voltage:\t\t{:.4f} V / V_gap\n"
+        str3 = "\tThev. impedance:\t{:.2f} ohms / R_N\n"
 
-        fout = open(filename, 'w')
-        fout.write("Embedding Circuit:\n")
-        fout.write("\tf = {0}\n".format(self.num_f))
-        fout.write("\tp = {0}\n".format(self.num_p))
-        if self.fgap is None or self.vgap is None or self.rn is None:
+        with open(filename, 'w') as fout:
+            fout.write("Embedding Circuit:\n")
+            fout.write("\tNumber of tones:     {0}\n".format(self.num_f))
+            fout.write("\tNumber of harmonics: {0}\n".format(self.num_p))
+
             for f in range(1, self.num_f + 1):
                 for p in range(1, self.num_p + 1):
                     vph = self.vph[f]
                     vt = self.vt[f, p]
                     zt = self.zt[f, p]
                     fout.write(str1.format(f, p, vph))
-                    fout.write(str3.format(float(vt.real)))
-                    fout.write(str4.format(zt))
-        else:
-            for f in range(1, self.num_f + 1):
-                for p in range(1, self.num_p + 1):
-                    fq = self.vph[f] * self.fgap / 1e9
-                    vt = self.vt[f, p]
-                    zt = self.zt[f, p]
-                    power = self.available_power(f, p)
-                    with np.errstate(divide='ignore'):
-                        power_dbm = 10 * np.log10(power * 1000)
-                    fout.write(str2.format(f, p, fq))
-                    fout.write(str3.format(float(vt.real)))
-                    fout.write(str6.format(float(vt.real / (self.vph[f] * p))))
-                    fout.write(str4.format(zt))
-                    fout.write(str5.format(power / 1e-9))
-                    fout.write(str7.format(power_dbm))
+                    fout.write(str2.format(vt))
+                    fout.write(str3.format(zt))
 
     def lock(self):
         """Lock down all numpy arrays, so that errors will be raised if
@@ -314,23 +314,15 @@ class EmbeddingCircuit(object):
 
         """
 
-        self._num_f = self.num_f
-        self._num_p = self.num_p
-        self._num_n = self.num_n
-
         self.vph.flags.writeable = False
         self.vt.flags.writeable = False
         self.zt.flags.writeable = False
         self.vb.flags.writeable = False
 
     def unlock(self):
-        """Unlock numpy arrays and ensure no constants were changed.
+        """Unlock numpy arrays.
 
         """
-
-        assert self._num_f == self.num_f
-        assert self._num_p == self.num_p
-        assert self._num_n == self.num_n
 
         self.vph.flags.writeable = True
         self.vt.flags.writeable = True
@@ -338,10 +330,10 @@ class EmbeddingCircuit(object):
         self.vb.flags.writeable = True
 
 
-def read_cct(filename):
+def read_circuit(filename):
     """ Read-in embedding circuit information.
 
-    Set by EmbeddingCircuit.save_info(filename)
+    Set previously by EmbeddingCircuit.save_info(filename)
 
     Args:
         filename: filename
@@ -367,35 +359,10 @@ def read_cct(filename):
             f = int(re.search(r'\d+', split_line[0]).group())
             p = int(re.search(r'\d+', split_line[1]).group())
             vph = float(split_line[4])
-            vt = float(data[i + 1].split()[2])
+            vt = complex(data[i + 1].split()[2])
             zt = complex(data[i + 2].split()[2])
             cct.vph[f] = vph
             cct.vt[f, p] = vt
             cct.zt[f, p] = zt
 
     return cct
-
-
-# RUN ------------------------------------------------------------------------
-
-# def _main():
-#
-#     # Build a made up circuit
-#     cct = EmbeddingCircuit(2, 1, fgap=650e9, vgap=2.8e-3, rn=13.5, name='Test')
-#     cct.comment[1][1] = 'LO'
-#     cct.comment[2][1] = 'USB'
-#     cct.vph[1] = 0.3
-#     cct.vph[2] = 0.32
-#     cct.zt[1] = 0.3 - 1j*0.3
-#     cct.zt[2] = 0.3 - 1j*0.3
-#     cct.set_available_power(100e-9, 1, 1)
-#     cct.set_available_power(0.1e-9, 2, 1)
-#
-#     cct.print_info()
-#
-#     print cct
-#
-#
-# if __name__ == "__main__":
-#
-#     _main()
