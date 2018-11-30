eae1a4ee0e42a682eda0b4f50e121e4ca868ffaa
John Garrett <john.garrett@astro.ox.ac.uk>
2018-11-30 23:42:19 +0000

    Fix exponential IV model and add tests for IV models

--- qmix/mathfn/ivcurve_models.py
+++ qmix/mathfn/ivcurve_models.py
@@ -1,35 +1,23 @@
-"""I-V curve models.
+"""Models to generate DC I-V curves.
 
-These models range from simple (e.g., the 'perfect' and the 'polynomial'
-models) to complex (e.g., the 'exponential', and 'expanded').
-
-Warning:
-
-   - They all use normalized voltage/current *except* for 'expanded'.
-
-References:
-
-   - Polynomial model:
-   
-      Kennedy's 1999 thesis (?)
-      
-   - Exponential model:
-
-      H. Rashid, et al., "Harmonic and reactive behavior of the
-      quasiparticle tunnel current in SIS junctions," AIP Advances,
-      vol. 6, 2016.
+These models range from simple (e.g., 'perfect' and 'polynomial') to 
+complex (e.g., 'exponential' and 'expanded').
 
 """
 
 import numpy as np
-# from scipy.optimize import curve_fit
-# from qmix.mathfn.misc import slope
 
 
-# I-V curve models ------------------------------------------------------------
+# I-V curve models -----------------------------------------------------------
 
 def perfect(voltage):
     """Perfect I-V curve.
+
+    This is the ideal I-V curve. The current is equal to 0 when |Vb| < 1, and 
+    equal to 0.5 when |Vb| = 1 (where Vb is the bias voltage). Otherwise, the 
+    normalized current is equal to the normalized voltage (normalized to Igap 
+    and Vgap, respectively, where Igap=Vgap/Rn is the gap current, Vgap is the
+    gap voltage, and Rn is the normal resistance).
 
     Args:
         voltage (ndarray): normalized bias voltage
@@ -40,14 +28,15 @@ def perfect(voltage):
     """
 
     current = np.copy(voltage)
-    current[np.abs(voltage) == 1] = 0.5
+    current[voltage == 1.] = 0.5
+    current[voltage == -1.] = -0.5
     current[np.abs(voltage) < 1] = 0
 
     return current
 
 
-def perfect_kk(voltage):
-    """Kramers-Kronig transform of perfect i-v curve.
+def perfect_kk(voltage, max_kk=100.):
+    """Kramers-Kronig transform of the perfect I-V curve.
 
     Args:
         voltage (ndarray): normalized bias voltage
@@ -57,20 +46,28 @@ def perfect_kk(voltage):
 
     """
 
-    current_kk = -1 / np.pi * (2 + voltage * (np.log(np.abs((voltage - 1) / (voltage + 1)))))
+    kk = np.empty_like(voltage, dtype=float)
 
-    # np.log(v-1) will cause result=Nan at v=1, replace with 100 instead
-    current_kk[np.abs(voltage) == 1] = 100
+    mask = np.abs(voltage) != 1.
+    kk[mask] = (-1 / np.pi * (2 + voltage[mask] * 
+               (np.log(np.abs((voltage[mask] - 1) / 
+               (voltage[mask] + 1))))))
 
-    return current_kk
+    # np.log(v-1) will cause result=Nan at v=-1 and v=1
+    # replace with max value instead
+    kk[np.invert(mask)] = max_kk
+
+    return kk
 
 
 def polynomial(voltage, order=50):
     """Polynomial I-V curve model.
 
+    From Kennedy's thesis (1999).
+
     Args:
         voltage (ndarray): normalized bias voltage
-        order (float): polynomial order (usually between 30 and 50)
+        order (float): order of polynomial (usually between 30 and 50)
 
     Returns:
         ndarray: normalized current
@@ -82,19 +79,33 @@ def polynomial(voltage, order=50):
     return current
 
 
-def exponential(voltage, vgap=2.75e-3, rn=13.5, rsg=300., agap=5e4):
-    """Exponential I-V curve model from Chalmers
+def exponential(voltage, vgap=2.8e-3, rn=14., rsg=300., agap=4e4, model='fixed'):
+    """The exponential I-V curve model that is used in Chalmers papers.
 
-    Note that the normal resistance will be slightly lower than 'rn', which is
-    mistake in their formula... I keep this version so that I can recreate
-    their data.
+    Reference: 
+        H. Rashid, S. Krause, D. Meledin, V. Desmaris, A. Pavolotsky, and 
+        V. Belitsky, "Frequency Multiplier Based on Distributed 
+        Superconducting Tunnel Junctions: Theory, Design, and 
+        Characterization," IEEE Trans. Terahertz Sci. Technol., pp. 1-13, 
+        2016.
+
+    Note:
+        - The equation from this paper will result in an I-V curve that has a
+          subgap resistance that is half the value that it is supposed to be.
+        - The normal resistance will also be slightly lower than it is
+          supposed to be.
+        - I fixed this model. This model can be selected by setting 
+          model='fixed'. The original model can be selected by 
+          setting model='original'.
+
 
     Args:
         voltage (ndarray): normalized bias voltage
-        vgap (float): gap voltage (in V)
-        rn (float): normal resistance (in ohms)
-        rsg (float): sub-gap resistance (in ohms)
+        vgap (float): gap voltage, in units [V]
+        rn (float): normal resistance, in units [ohms]
+        rsg (float): sub-gap resistance, in units [ohms]
         agap (float): gap linearity coefficient (typically around 4e4)
+        model (str): model to used (either 'fixed' or 'original')
 
     Returns:
         ndarray: normalized current
@@ -102,39 +113,59 @@ def exponential(voltage, vgap=2.75e-3, rn=13.5, rsg=300., agap=5e4):
     """
 
     igap = vgap / rn
-    v_v = voltage * vgap  # un-normalized bias voltage
+    v_v = voltage * vgap  # voltage in units [V]
 
-    np.seterr(over='ignore')
-    i_a = (v_v / rsg * (1 / (1 + np.exp(-agap * (v_v + vgap)))) +
-           v_v / rn * (1 / (1 + np.exp(agap * (v_v + vgap))))) + \
-          (v_v / rsg * (1 / (1 + np.exp(agap * (v_v - vgap)))) +
-           v_v / rn * (1 / (1 + np.exp(-agap * (v_v - vgap)))))
+    if model.lower() == 'fixed':
+        np.seterr(over='ignore')
+        i_a = (
+               # Sub-gap resistance
+               v_v / (rsg * 2) * (1 / (1 + np.exp(-agap * (v_v + vgap)))) -
+               v_v / (rsg * 2) * (1 / (1 + np.exp( agap * (v_v + vgap)))) -
+               v_v / (rsg * 2) * (1 / (1 + np.exp(-agap * (v_v - vgap)))) +
+               v_v / (rsg * 2) * (1 / (1 + np.exp( agap * (v_v - vgap)))) +
+               # Normal resistance
+               v_v / rn  * (1 / (1 + np.exp( agap * (v_v + vgap)))) +
+               v_v / rn  * (1 / (1 + np.exp(-agap * (v_v - vgap)))))
+        return i_a / igap
+    elif model.lower() == 'original':
+        np.seterr(over='ignore')
+        i_a = (
+               # Sub-gap resistance
+               v_v / rsg * (1 / (1 + np.exp(-agap * (v_v + vgap)))) +
+               v_v / rsg * (1 / (1 + np.exp( agap * (v_v - vgap)))) +
+               # Normal resistance
+               v_v / rn  * (1 / (1 + np.exp( agap * (v_v + vgap)))) + 
+               v_v / rn  * (1 / (1 + np.exp(-agap * (v_v - vgap)))))
+        return i_a / igap
+    else:
+        raise ValueError("Model not recognized.")
 
-    return i_a / igap
 
+def expanded(voltage, vgap=2.8e-3, rn=14., rsg=5e2, agap=4e4, a0=1e4,
+             ileak=5e-6, vnot=2.85e-3, inot=1e-5, anot=2e4, ioff=1e-5):
+    """The expanded I-V curve model.
 
-def expanded(voltage, vgap=2.8e-3, rn=14, rsg=5e2, agap=5e4, a0=1e4,
-             ileak=5e-6, vnot=2.85e-3, inot=1e-5, anot=2e4, ioff=1.2e-5):
-    """Expanded I-V curve model.
+    This model is based on the I-V curve model from:
+        H. Rashid, S. Krause, D. Meledin, V. Desmaris, A. Pavolotsky, and 
+        V. Belitsky, "Frequency Multiplier Based on Distributed 
+        Superconducting Tunnel Junctions: Theory, Design, and 
+        Characterization," IEEE Trans. Terahertz Sci. Technol., pp. 1-13, 
+        2016.
 
-    Based on I-V curve model from Rashid et al. 2016. I have changed it
-    substantially to include leakage current, the proximity effect, the onset 
-    thermal tunnelling, and the reduced current amplitude often seen above the
-    gap.
-
-    It is able to capture experimental data quite well except for area just 
-    below the gap voltage.
+    I have added the ability to include leakage current, the proximity effect,
+    the onset thermal tunnelling, and the reduced current amplitude often seen
+    above the gap. It is able to recreate experimental data very well.
 
     Args:
         voltage (ndarray): normalized bias voltage
-        vgap (float): gap voltage
-        rn (float): normal resistance
-        rsg (float): subgap resistance
-        agap (float): linearity of transition at v_gap
-        a0 (float): linearity of 'zig-zag'
+        vgap (float): gap voltage, in units [V]
+        rn (float): normal resistance, in units [ohms]
+        rsg (float): sub-gap resistance, in units [ohms]
+        agap (float): gap linearity coefficient (typically around 4e4)
+        a0 (float): linearity coefficient at the origin
         ileak (float): amplitude of leakage current
-        vnot (float): notch location
-        inot (float): notch current amplitude
+        vnot (float): notch location, in units [V]
+        inot (float): notch current amplitude, in units [A]
         anot (float): linearity of notch
         ioff (float): current offset
 
@@ -152,14 +183,14 @@ def expanded(voltage, vgap=2.8e-3, rn=14, rsg=5e2, agap=5e4, a0=1e4,
         ileak * 2 * (1 / (1 + np.exp(-a0 * v_v))) -
         ileak * np.ones_like(voltage) -
         ileak * (1 / (1 + np.exp(-agap * (v_v - vgap)))) +
-        ileak * (1 / (1 + np.exp(agap * (v_v + vgap)))) +
+        ileak * (1 / (1 + np.exp( agap * (v_v + vgap)))) +
         # Sub-gap resistance
         v_v / (rsg * 2) * (1 / (1 + np.exp(-agap * (v_v + vgap)))) -
-        v_v / (rsg * 2) * (1 / (1 + np.exp(agap * (v_v + vgap)))) -
+        v_v / (rsg * 2) * (1 / (1 + np.exp( agap * (v_v + vgap)))) -
         v_v / (rsg * 2) * (1 / (1 + np.exp(-agap * (v_v - vgap)))) +
-        v_v / (rsg * 2) * (1 / (1 + np.exp(agap * (v_v - vgap)))) +
+        v_v / (rsg * 2) * (1 / (1 + np.exp( agap * (v_v - vgap)))) +
         # Transition and normal resistance
-        v_v / rn * (1 / (1 + np.exp(agap * (v_v + vgap)))) +
+        v_v / rn * (1 / (1 + np.exp( agap * (v_v + vgap)))) +
         v_v / rn * (1 / (1 + np.exp(-agap * (v_v - vgap)))) +
         # Notch above gap
         inot / (1 + np.exp(anot * (v_v - vnot))) +
@@ -170,91 +201,3 @@ def expanded(voltage, vgap=2.8e-3, rn=14, rsg=5e2, agap=5e4, a0=1e4,
         0)
 
     return i_a / igap
-
-
-# # Fit exp data to IV curve models --------------------------------------
-#
-# def fit_polynomial_model(voltage, current):
-#     """Fit polynomial model to exp data.
-#
-#     Warning:
-#
-#        - Experimental! Not very accurate.
-#
-#     Args:
-#         voltage (ndarray): normalized voltage
-#         current (ndarray): normalized current
-#
-#     Returns:
-#         float: approximate order of polynomial
-#
-#     """
-#
-#     print "Warning: fit_polynomial_model is experimental!"
-#
-#     der_max = np.max(slope(voltage, current))
-#
-#     order = der_max * 2. - 1.
-#
-#     return order
-#
-#
-# def fit_expanded(voltage, current, vgap=2.8e-3, rn=14, rsg=5e2, agap=5e4,
-#                  a0=1e4, ileak=5e-6, vnot=2.85e-3, inot=1e-5, anot=2e4,
-#                  ioff=1.2e-5):
-#     """Fit the 'expanded model' to experimental data.
-#
-#     Warning:
-#
-#        - Initial guess must be very close because there are so many parameters
-#          to fit.
-#
-#     Args:
-#         voltage (ndarray): normalized voltage
-#         current (ndarray): normalized current
-#         vgap (float): initial guess for gap voltage
-#         rn (float): initial guess for normal resistance
-#         rsg (float): initial guess for sub-gap resistance
-#         agap (float): initial guess for linearity of transition
-#         a0 (float): initial guess for linearity of zig-zag
-#         ileak (float): initial guess for leakage current amplitude
-#         vnot (float): initial guess for notch voltage
-#         inot (float): initial guess for notch current amplitude
-#         anot (float): initial guess for notch linearity
-#         ioff (float): initial guess for current offset
-#
-#     Returns:
-#         ndarray: parameters for the 'full model'
-#
-#     """
-#
-#     # Fit the full model to the experimental data
-#     popt, _ = curve_fit(expanded, voltage, current, p0=[vgap, rn, rsg, agap, a0, ileak, vnot, inot, anot, ioff])
-#
-#     # # Unpack
-#     # v_g_out = popt[0]
-#     # r_n_out = popt[1]
-#     # r_s_out = popt[2]
-#     # a_g_out = popt[3]
-#     # a_0_out = popt[4]
-#     # i_l_out = popt[5]
-#     # v_n_out = popt[6]
-#     # i_n_out = popt[7]
-#     # a_n_out = popt[8]
-#     # i_o_out = popt[9]
-#
-#     params = {
-#         'vgap'  : popt[0],
-#         'rn'    : popt[1],
-#         'rsg'   : popt[2],
-#         'agap'  : popt[3],
-#         'a0'    : popt[4],
-#         'ileak' : popt[5],
-#         'vnot'  : popt[6],
-#         'inot'  : popt[7],
-#         'anot'  : popt[8],
-#         'ioff'  : popt[9]
-#         }
-#
-#     # return v_g_out, r_n_out, a_0_out, i_l_out, r_s_out, a_g_out, v_n_out, i_n_out, a_n_out, i_o_out
-#     return params
---
+++ tests/test_ivcurve-models.py
@@ -0,0 +1,90 @@
+"""Test the I-V curve models (qmix.mathfn.ivcurve_models).
+
+"""
+
+import numpy as np
+
+import qmix.mathfn.ivcurve_models as iv
+from qmix.mathfn.misc import slope
+
+# Model parameters
+vgap  = 3e-3
+rn    = 14.
+rsg   = 5e2
+agap  = 5e4
+a0    = 1e4
+ileak = 5e-6
+vnot  = 2.85e-3
+inot  = 1e-5
+anot  = 2e4
+ioff  = 1.2e-5
+
+
+def test_perfect_iv_model():
+    """Test the `perfect' I-V curve model."""
+
+    # Test I-V curve model
+    x = np.array([-2., -1., 0., 1., 2.])
+    y = iv.perfect(x)
+    np.testing.assert_equal(y, [-2., -0.5, 0., 0.5, 2.])
+
+    # Test KK transform
+    x = np.array([-1e10, -1, 1, 1e10])
+    y = iv.perfect_kk(x)
+    assert np.abs(y[0]) <  1e-7
+    assert np.abs(y[1]) ==  100.
+    assert np.abs(y[2]) ==  100.
+    assert np.abs(y[3]) <  1e-7
+
+
+def test_exponential_model():
+    """Test the exponential I-V curve model that is used by the Chalmers goup.
+
+    For example, see:
+
+        H. Rashid, S. Krause, D. Meledin, V. Desmaris, A. Pavolotsky, and 
+        V. Belitsky, "Frequency Multiplier Based on Distributed 
+        Superconducting Tunnel Junctions: Theory, Design, and 
+        Characterization," IEEE Trans. Terahertz Sci. Technol., pp. 1-13, 
+        2016.
+
+    """
+
+    # Generate I-V curve
+    x = np.linspace(-3, 3, 201)
+    y = iv.exponential(x, vgap=vgap, rn=rn, rsg=rsg, agap=agap)
+    x *= vgap
+    y *= (vgap / rn)
+
+    # Calculate resistance
+    dxdy = slope(y, x)
+
+    # Check subgap resistance
+    idx = np.abs(x).argmin()
+    assert round(dxdy[idx], 10) == rsg
+
+    # Check normal resistance
+    idx = np.abs(x - 7.5e-3).argmin()
+    assert round(dxdy[idx], 10) == rn
+
+
+def test_expanded_model():
+    """Test the expanded I-V curve model."""
+
+    # Generate I-V curve
+    x = np.linspace(-2, 2, 201)
+    y = iv.expanded(x, vgap=vgap, rn=rn, rsg=rsg, agap=agap, a0=a0,
+                    ileak=ileak, vnot=vnot, inot=inot, anot=anot, ioff=ioff)
+    x *= vgap
+    y *= (vgap / rn)
+
+    # Calculate resistance
+    dxdy = slope(y, x)
+
+    # Calculate sub-gap resistance
+    idx = np.abs(x - 2e-3).argmin()
+    assert round(dxdy[idx], 2) == rsg
+
+    # Calculate normal resistance
+    idx = np.abs(x - 5e-3).argmin()
+    assert round(dxdy[idx], 2) == rn
