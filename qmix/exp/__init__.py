fe83c78300960879f875c3d718a8ea9bc84118cd
John Garrett <john.garrett@astro.ox.ac.uk>
2019-03-01 13:06:17 +0000

    Remove old functions from import (previously commented out) [ci skip]

--- qmix/exp/__init__.py
+++ qmix/exp/__init__.py
@@ -3,5 +3,3 @@ experimental data. """
 
 from qmix.exp.exp_data import (RawData, RawData0,
                                plot_overall_results, plot_if_spectrum)
-#, check_iv_if_matching)
-#, generate_filename_from_f)
